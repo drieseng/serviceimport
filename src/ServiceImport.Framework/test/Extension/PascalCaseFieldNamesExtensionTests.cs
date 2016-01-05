645c12482ba229296e94bac571ea3c7be9ebc3bd
drieseng <gert.driesen@telenet.be>
2016-01-05 21:45:52 +0100

    Added place holders for some PascalCaseFieldNamesExtension tests. Ignore nuget packages, Visual Studio output files and Visual Studio temp files.

---
+++ src/ServiceImport.Framework/test/Extension/PascalCaseFieldNamesExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace BRail.Nis.ServiceImport.Framework.Tests.Extension
+{
+    [TestFixture]
+    public class PascalCaseFieldNamesExtensionTests
+    {
+        [Test]
+        public void PrivateInstanceFieldsThatDoNotHavePascalCaseFieldNamesAreRenamed()
+        {
+        }
+
+        [Test]
+        public void ReferencesToPrivateInstanceFieldsThatDoNotHavePascalCaseFieldNamesAreUpdated()
+        {
+        }
+
+        [Test]
+        public void PublicInstanceFieldsThatDoNotHavePascalCaseFieldNamesAreNotRenamed()
+        {
+        }
+    }
+}
